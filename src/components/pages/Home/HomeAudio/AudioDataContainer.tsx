f094b777ec28d5eca9749a9bc3694cb30cac3cc3
Matt Dean <mdean400@gmail.com>
2022-05-06 22:56:03 -0600

    Only render AudioVisualizer when it should be playing

--- src/components/pages/Home/HomeAudio/AudioDataContainer.tsx
+++ src/components/pages/Home/HomeAudio/AudioDataContainer.tsx
@@ -23,12 +23,53 @@
 // https://betterprogramming.pub/using-react-ui-components-to-visualize-real-time-spectral-data-of-an-audio-source-17a498a6d8d7
 // https://github.com/matt-eric/web-audio-fft-visualization-with-react-hooks
 
+import { attributes as HomeContentAttributes } from '@content/home.md';
+import { Box, Button, Heading, Stack } from 'grommet';
+import { Microphone, PauseFill } from 'grommet-icons';
 import React from 'react';
+import styled from 'styled-components';
 import AudioVisualizer from './AudioVisualizer';
+
+const SmallableSpan = styled.span`
+  @media screen and (max-width: 400px) {
+    font-size: 5vw;
+    line-height: 5vw;
+  }
+`;
+
+const ButtonWithIcon = styled(Button)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+`;
+
+const TaglineContainer = styled.div`
+  height: 100%;
+  background-color: var(--status-ok);
+  color: white;
+  clip-path: polygon(17% 0, 100% 0, 100% 100%, 0% 100%);
+  width: 300px;
+  display: flex;
+  justify-content: flex-end;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    background-color: #db7f00;
+  }
+`;
+
+const NewHeading = styled(Heading)`
+  @media screen and (max-width: 400px) {
+    font-size: 12vw;
+    line-height: 13vw;
+  }
+`;
 
 export default function AudioDataContainer() {
   const audioFile = React.useRef<HTMLAudioElement>(undefined);
   const audioData = React.useRef<AnalyserNode>(undefined);
+  const [isPlaying, setIsPlaying] = React.useState(false);
 
   function initializeAudioAnalyser() {
     audioFile.current = new Audio();
@@ -46,10 +87,12 @@ export default function AudioDataContainer() {
     source.connect(audioData.current);
 
     audioFile.current.play();
+    setIsPlaying(true);
   }
 
   function pause() {
     audioFile.current.pause();
+    setIsPlaying(false);
   }
 
   function getFrequencyData(styleAdjuster) {
@@ -59,11 +102,61 @@ export default function AudioDataContainer() {
     styleAdjuster(amplitudeArray);
   }
 
+  function toggleAudio() {
+    if (isPlaying) {
+      pause();
+      setIsPlaying(false);
+    } else {
+      initializeAudioAnalyser();
+      setIsPlaying(true);
+    }
+  }
+
   return (
-    <AudioVisualizer
-      initializeAudioAnalyser={initializeAudioAnalyser}
-      getFrequencyData={getFrequencyData}
-      pause={pause}
-    />
+    <>
+      <div>
+        <Stack guidingChild='last'>
+          {isPlaying && <AudioVisualizer getFrequencyData={getFrequencyData} />}
+          <Box background={{ color: 'brand', opacity: 'weak' }} height='100%'>
+            <NewHeading
+              level={1}
+              size='large'
+              margin='large'
+              textAlign='center'
+            >
+              {HomeContentAttributes.hero_main_text}
+            </NewHeading>
+          </Box>
+        </Stack>
+      </div>
+      <Box
+        direction='row'
+        justify='between'
+        align='center'
+        height='60px'
+        background={{ color: 'white' }}
+      >
+        <Box style={{ paddingLeft: '20px' }}>
+          <SmallableSpan>{HomeContentAttributes.hero_sub_text}</SmallableSpan>
+        </Box>
+        <TaglineContainer
+          style={{ paddingRight: '20px' }}
+          onClick={toggleAudio}
+        >
+          <ButtonWithIcon>
+            <div style={{ textAlign: 'right' }}>
+              <SmallableSpan>
+                {HomeContentAttributes.audio_sample_text}
+              </SmallableSpan>
+            </div>
+            {isPlaying ? (
+              <PauseFill color='white'></PauseFill>
+            ) : (
+              <Microphone color='white'></Microphone>
+            )}
+          </ButtonWithIcon>
+        </TaglineContainer>
+      </Box>
+    </>
   );
 }
